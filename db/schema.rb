b83df339a4d957ebbf8fa211aa9baee085cef3aa
amaury ropert <amauryropert@gmail.com>
2019-05-29 20:25:44 +0200

    add numbers to like

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_143529) do
+ActiveRecord::Schema.define(version: 2019_05_29_182347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_05_29_143529) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number", default: 0
     t.index ["game_id"], name: "index_likes_on_game_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
