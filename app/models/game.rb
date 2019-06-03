34e9fa87710a5314f8acdfddac13ed8d1a4cc7d4
amaury ropert <amauryropert@gmail.com>
2019-06-03 14:56:18 +0200

    models et routes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :users, through: :likes
   validates :name, presence: true, allow_blank: false
   validates :platform, presence: true
 end
