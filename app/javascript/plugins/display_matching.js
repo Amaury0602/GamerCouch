66db1192b106fd5c2e01c0e1b697de4a861644f7
charles degrassat <charles.degrassat@gmail.com>
2019-06-07 15:49:05 +0200

    Fixed the multiple like problem. Still fixing the like count problem...

--- app/javascript/plugins/display_matching.js
+++ app/javascript/plugins/display_matching.js
@@ -1,21 +1,33 @@
 const displayMatching = () => {
-
   const displayButtons = document.querySelectorAll(".right_choice")
-  if(displayButtons) {
     displayButtons.forEach((button) => {
       button.addEventListener('click', (event) => {
         event.preventDefault()
         const gameID = button.attributes['data-game-id'].value
         const hiddenDiv = document.querySelector(`[data-hidden-id='${gameID}']`)
-        button.firstChild.classList.toggle("active");
-        if (hiddenDiv.style.display === "block") {
-              hiddenDiv.style.display = "none";
-            } else {
-              hiddenDiv.style.display = "block";
-            }
+        button.classList.toggle("active")
+        if (hiddenDiv.classList.contains("hidden") ) {
+          const matchingDivs = document.querySelectorAll(".hidden-match-index")
+          matchingDivs.forEach(matchingDiv => {
+            matchingDiv.classList.add("hidden")
+          })
+          const recommendationDivs = document.querySelectorAll(".recommandation-index")
+          recommendationDivs.forEach(recommendationDiv => {
+          recommendationDiv.classList.add("hidden")
+          })
+          hiddenDiv.classList.remove("hidden")
+        } else {
+          hiddenDiv.classList.add("hidden")
+        }
       })
     })
   }
-}
 
 export default displayMatching;
+
+
+// if (hiddenDiv.style.display === "block") {
+//               hiddenDiv.style.display = "none";
+//             } else {
+//               hiddenDiv.style.display = "block";
+//             }
--- app/javascript/plugins/display_recommandation.js
+++ app/javascript/plugins/display_recommandation.js
@@ -6,11 +6,16 @@ const displayRecommandation = () => {
       const gameID = button.attributes['data-game-id'].value
       const hiddenDiv = document.querySelector(`[data-hidden-reco-id='${gameID}']`)
       if (hiddenDiv.classList.contains("hidden")) {
+        const matchingDivs = document.querySelectorAll(".hidden-match-index")
+        matchingDivs.forEach(matchingDiv => {
+          matchingDiv.classList.add("hidden")
+        })
         const recommendationDivs = document.querySelectorAll(".recommandation-index")
         recommendationDivs.forEach(recommendationDiv => {
           recommendationDiv.classList.add("hidden")
         })
         hiddenDiv.classList.remove("hidden")
+
       } else {
         hiddenDiv.classList.add("hidden")
       }
@@ -20,3 +25,7 @@ const displayRecommandation = () => {
 
 
 export default displayRecommandation;
+
+// To do : faire la surlignement lorsqu'on clique sur le boutton
+// button.classList.remove("active")
+// button.classList.add("active")
