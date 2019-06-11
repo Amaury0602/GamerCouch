1b8660525f62b01c9876c4e1f4ce3ab64be57aa3
amaury ropert <amauryropert@gmail.com>
2019-06-11 19:00:40 +0200

    added colors to score need to finish

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,12 @@ import { displayRecommandation } from "../plugins/display_recommandation";
 import { displayLikeCounter } from "../plugins/display_like_counter";
 import { displayComment } from "../plugins/display_comment";
 import { select2Home } from "../plugins/select2_home";
+import { matchingStyle } from "../plugins/matching_style";
 
 displayMatching();
 displayRecommandation();
 select2Home();
 // displayLikeCounter();
 displayComment();
+matchingStyle();
 
