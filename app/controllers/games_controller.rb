a495bc2cd37719f9bd1dec4bdf8272bf1ae7de99
charles degrassat <charles.degrassat@gmail.com>
2019-06-11 19:08:53 +0200

    fix the right game for your

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,5 @@
 class GamesController < ApplicationController
   skip_after_action :verify_policy_scoped, :only => :index
-  skip_before_action :verify_authenticity_token, except: [:index, :show]
 
   def index
     if params[:sort] == "like"
@@ -8,8 +7,10 @@ class GamesController < ApplicationController
     elsif params[:sort] == "comment"
       @games = Game.order("comment_count DESC")
     else
-      @games = Game.all.sort { |a, b| a.matching_score(current_user) <=> b.matching_score(current_user) }.reverse
+      @games = Game.where.not(id: current_user.liked_games.pluck(:id))
+      @games = @games.sort { |a, b| a.matching_score(current_user) <=> b.matching_score(current_user) }.reverse
     end
+
     if params[:search_query].present?
       @games = @games.search_all(params[:search_query])
     end
@@ -20,22 +21,4 @@ class GamesController < ApplicationController
     authorize @game
     @comment = Comment.new
   end
-
-  def tracking
-    @games = Game.find(params[:tracking])
-    @user = current_user
-    @games.each do |game|
-      authorize game
-      @like = Like.new(game_id: game.id, user_id: @user.id)
-
-      @like.save
-    end
-    redirect_to games_path
-end
-  def game_alike
-    @game = Game.find(params[:tracking].to_i)
-    authorize @game
-    @three_most_liked_games = @game.three_most_liked
-  end
-
 end
