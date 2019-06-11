48a83056868b9149a0899bc5123979e024be8c7a
charles degrassat <charles.degrassat@gmail.com>
2019-06-11 11:46:59 +0200

    ajax score

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,6 @@ class Game < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :users, through: :likes
-  has_many :comment_users, through: :comments, source: :users
   validates :name, presence: true, allow_blank: false
   validates :platform, presence: true
   mount_uploader :photo, PhotoUploader
