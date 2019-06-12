a4fce4804048986a307b108c3954d63cc87d0299
Yordano Contreras <yordanoc.pro@outlook.com>
2019-06-12 12:51:40 +0200

    show page

--- app/javascript/plugins/image_show.js
+++ app/javascript/plugins/image_show.js
@@ -1,5 +1,5 @@
 const imageShow = () => {
- let bigImage = document.querySelector(".big-image");
+ let bigImage = document.querySelector(".big-image-bot");
   const littleImages = document.querySelectorAll(".little-images");
   littleImages.forEach ((image)=> {
       image.addEventListener("click" , (event) => {
