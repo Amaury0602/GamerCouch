a27ee7f9c08844ddd1825748924d074aefa07200
charles degrassat <charles.degrassat@gmail.com>
2019-06-13 16:01:38 +0200

    css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import { displayRecommandation } from "../plugins/display_recommandation";
 import { displayLikeCounter } from "../plugins/display_like_counter";
 import { displayComment } from "../plugins/display_comment";
 import { select2Home } from "../plugins/select2_home";
-import { matchingStyle } from "../plugins/matching_style";
+// import { matchingStyle } from "../plugins/matching_style";
 import { imageShow } from "../plugins/image_show";
 
 displayMatching();
@@ -14,6 +14,6 @@ displayRecommandation();
 select2Home();
 // displayLikeCounter();
 displayComment();
-matchingStyle();
+// matchingStyle();
 imageShow();
 
