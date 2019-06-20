d9fb427eeb172f4612f48046dce4e6e6e697ec1d
amaury ropert <amauryropert@gmail.com>
2019-06-20 16:56:42 +0200

    test sql NE PAS PUSH

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,7 @@ class Game < ApplicationRecord
   has_many :wishes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :users, through: :likes
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true, allow_blank: false
   validates :platform, presence: true
   mount_uploader :photo, PhotoUploader
@@ -15,23 +16,34 @@ class Game < ApplicationRecord
     }
 
   def match(user)
-    other_users = []
-    self.likes.each do |like|
-      other_users << like.user
-    end
-    other_liked_games = []
-    other_users.each do |user|
-      user.likes.each do |like|
-        other_liked_games << like.game
+    # other_users = []
+    # self.likes.each do |like|
+    #   other_users << like.user
+    # end
+    # @other_liked_games = []
+    # other_users.each do |user|
+    #   user.likes.each do |like|
+    #     @other_liked_games << like.game
+    #   end
+    # end
+    # @matchings = user.games_liked & @other_liked_games
+    # @matchings = @matchings.reject { |game| game == self }
+    users = Like.where("game_id= ?", self.id).pluck(:user_id)
+    if !users.empty?
+      other_sql = ActiveRecord::Base.connection.execute("SELECT games.id FROM games
+                                                         JOIN likes ON games.id = likes.game_id
+                                                         WHERE likes.user_id IN (#{users.join(",")})").values.flatten
+      my_sql = ActiveRecord::Base.connection.execute("SELECT games.id FROM games
+                                                      JOIN likes ON games.id = likes.game_id
+                                                      WHERE likes.user_id = #{user.id}").values.flatten
+      @matchings = other_sql.map { |g_id| Game.find(g_id) } & my_sql.map{ |g_id| Game.find(g_id) }
+      @matchings.reject { |instance| instance == self }
+      hash = {}
+      @matchings.each do |jeu|
+        hash[jeu.name] = jeu.likes.where(user_id: self.users).size.fdiv(self.likes.size) * 100
       end
+      hash = hash.sort_by { |game, score| score }.reverse
     end
-    @matchings = user.games_liked & other_liked_games
-    @matchings = @matchings.reject { |game| game == self }
-    hash = {}
-    @matchings.each do |game|
-      hash[game] = game.likes.where(user_id: self.users).size.fdiv(likes.size) * 100
-    end
-    hash = hash.sort_by { |game, score| score }.reverse
   end
 
   def three_most_liked
@@ -52,14 +64,27 @@ def matching_score(user)
   total = []
   result = 0
   n = 0
-  self.match(user).each do |game|
-    total << game[1]
-  end
-  total.sort.each do |score|
-    n += 1
-    result += score * n
-  end
-  a = (1..n).sum
-  a == 0 ? result : (result / a).round(0)
+  if self.nil?
+    self.match(user).each do |game|
+      total << game[1]
+    end
+    total.sort.each do |score|
+      n += 1
+      result += score * n
+    end
+    a = (1..n).sum
+    a == 0 ? result : (result / a).round(0)
+    end
   end
 end
+
+
+
+
+# game, tu prends ses users
+
+# Ces users: tu prends leurs likes.game.name
+
+# on croise mes likes + les jeux likés par les users (pour un jeu donné) game1, game2, game3
+
+# game : game1.likes.count / game.likes.count
